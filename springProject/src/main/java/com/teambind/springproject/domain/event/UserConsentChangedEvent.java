73cd81bf8d80ab0f17b2b1168205363ca69b2f73
ddingjoo <ddingsha9@teambind.co.kr>
2025-12-12 14:26:23 +0900

    [STYLE] UserConsentChangedEvent 코드 포맷 정리

--- springProject/src/main/java/com/teambind/springproject/domain/event/UserConsentChangedEvent.java
+++ springProject/src/main/java/com/teambind/springproject/domain/event/UserConsentChangedEvent.java
@@ -1,10 +1,6 @@
 package com.teambind.springproject.domain.event;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 /**
  * Auth Server에서 발행하는 동의 변경 이벤트
@@ -16,9 +12,9 @@ import lombok.Setter;
 @NoArgsConstructor
 @Builder
 public class UserConsentChangedEvent {
-
-    private String userId;
-    private String consentId;
-    private boolean consented;
-    private String changedAt;
+	
+	private String userId;
+	private String consentId;
+	private boolean consented;
+	private String changedAt;
 }
