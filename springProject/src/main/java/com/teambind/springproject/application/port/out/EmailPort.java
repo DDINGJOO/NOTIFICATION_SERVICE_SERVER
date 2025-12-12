1f048e14eb803982734caf165b015dc83ad83f4f
ddingjoo <ddingsha9@teambind.co.kr>
2025-12-12 14:26:01 +0900

    [STYLE] EmailPort 코드 포맷 정리

--- springProject/src/main/java/com/teambind/springproject/application/port/out/EmailPort.java
+++ springProject/src/main/java/com/teambind/springproject/application/port/out/EmailPort.java
@@ -1,8 +1,8 @@
 package com.teambind.springproject.application.port.out;
 
 public interface EmailPort {
-
-    void send(String to, String subject, String htmlContent);
-
-    void sendVerificationEmail(String to, String code);
+	
+	void send(String to, String subject, String htmlContent);
+	
+	void sendVerificationEmail(String to, String code);
 }
